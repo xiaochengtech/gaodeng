8efaf335cd5a297491d3982eb20dcf82f3e3482d
leexsoft-mac <leexsoft@163.com>
2019-08-14 19:44:48 +0800

    修改sdk的目录结构

--- client.go
+++ client.go
@@ -4,7 +4,8 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"gaodeng/goland"
+
+	golden "gaodeng/golden/1.0.0"
 )
 
 // 高灯客户端的构造函数
@@ -16,14 +17,14 @@ func NewClient(isProd bool) (client *Client) {
 		client.env = EnvTest
 	}
 	client.config = NewConfig(isProd)
-	client.sdk = goland.NewSdk(client.config.AppKey, client.config.AppSecret, AppVersion, client.env)
+	client.sdk = golden.NewSdk(client.config.AppKey, client.config.AppSecret, AppVersion, client.env)
 	return client
 }
 
 type Client struct {
 	env    string      // 是否是生产环境
 	config Config      // 配置信息
-	sdk    *goland.Sdk // 高灯开票sdk
+	sdk    *golden.Sdk // 高灯开票sdk
 }
 
 // 向高灯sdk发送请求
--- golden/1.0.0/sdk.go
+++ golden/1.0.0/sdk.go
@@ -1,4 +1,4 @@
-package goland
+package golden
 
 import (
 	"bytes"
