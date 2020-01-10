d4e8447e4d58ca4ad0663aa89a299097f7ea9602
leexsoft-mac <leexsoft@163.com>
2020-01-10 16:01:40 +0800

    修改返回值

--- model.go
+++ model.go
@@ -2,7 +2,7 @@ package gaodeng
 
 // 返回结果的通信标识
 type ResponseModel struct {
-	Code uint16      `json:"code"` // 状态码
-	Msg  string      `json:"msg"`  // 错误信息
-	Data interface{} `json:"data"` // 业务逻辑返回
+	Code uint16      `json:"code"`    // 状态码
+	Msg  string      `json:"message"` // 错误信息
+	Data interface{} `json:"data"`    // 业务逻辑返回
 }
